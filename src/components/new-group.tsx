14befe4b31e2553e49df3c7509cf9d0f9b36b3d6
frostime <frostime@foxmail.com>
2024-07-11 13:31:35 +0800

    ✨ feat: New Group 面板添加过渡动画

--- src/components/new-group.tsx
+++ src/components/new-group.tsx
@@ -4,6 +4,7 @@ import ItemWrap from "@/libs/components/item-wrap";
 import InputItem from "@/libs/components/item-input";
 
 import { i18n } from "@/utils/i18n";
+import { Transition } from "solid-transition-group";
 
 interface IPrpos {
     setGroup: (arg: { name?: string, type?: TBookmarkGroupType }) => void;
@@ -58,80 +59,107 @@ const NewGroup = (props: IPrpos) => {
                 }
             }}
         >
-            <ItemWrap
-                title={i18n_.name[0]}
-                description={i18n_.name[1]}
-            >
-                <InputItem
-                    key="name"
-                    value=""
-                    type="textinput"
-                    changed={(v) => {
-                        props.setGroup({ name: v });
-                    }}
-                />
-            </ItemWrap>
-            <ItemWrap
-                title={i18n_.type[0]}
-                description={i18n_.type[1]}
-            >
-                <InputItem
-                    key="type"
-                    value={groupType()}
-                    type="select"
-                    options={{
-                        normal: i18n.bookmarktype.normal,
-                        dynamic: i18n.bookmarktype.dynamic,
-                        // composed: i18n.bookmarktype.composed
-                    }}
-                    changed={(v) => {
-                        props.setGroup({ type: v });
-                        setGroupType(v);
-                        if (v !== 'normal') {
-                            props.setRule({ type: ruleType() });
-                        }
-                    }}
-                />
-            </ItemWrap>
-            <Show when={groupType() !== 'normal'}>
+            <div style={{ display: "flex", "flex-direction": 'column' }}>
                 <ItemWrap
-                    title={i18n_.rtype[0]}
-                    description={i18n_.rtype[1]}
+                    title={i18n_.name[0]}
+                    description={i18n_.name[1]}
                 >
                     <InputItem
-                        key="ruleType"
-                        value={ruleType()}
+                        key="name"
+                        value=""
+                        type="textinput"
+                        changed={(v) => {
+                            props.setGroup({ name: v });
+                        }}
+                    />
+                </ItemWrap>
+                <ItemWrap
+                    title={i18n_.type[0]}
+                    description={i18n_.type[1]}
+                >
+                    <InputItem
+                        key="type"
+                        value={groupType()}
                         type="select"
                         options={{
-                            sql: i18n.ruletype.sql,
-                            backlinks: i18n.ruletype.backlinks,
-                            attr: i18n.ruletype.attr,
+                            normal: i18n.bookmarktype.normal,
+                            dynamic: i18n.bookmarktype.dynamic,
+                            // composed: i18n.bookmarktype.composed
                         }}
                         changed={(v) => {
-                            props.setRule({ type: v });
-                            setRuleType(v);
+                            props.setGroup({ type: v });
+                            setGroupType(v);
+                            if (v !== 'normal') {
+                                props.setRule({ type: ruleType() });
+                            }
                         }}
                     />
                 </ItemWrap>
-                <ItemWrap
-                    title={i18n_.rinput}
-                    description={aboutRule().desc}
-                    //@ts-ignore
-                    direction={aboutRule().direction}
-                >
-                    <InputItem
-                        key="ruleInput"
-                        value=''
-                        //@ts-ignore
-                        type={aboutRule().input}
-                        changed={(v) => {
-                            props.setRule({ input: v });
-                        }}
-                        // nofnSize={true}
-                        flex1={ruleType() === 'attr' ? true : false}
-                    />
-                </ItemWrap>
-            </Show>
+            </div>
+            <Transition
+                onExit={(el, done) => {
+                    const a = el.animate({
+                        opacity: [1, 0],
+                        transform: ['translateY(0)', 'translateY(-5px)'],
+                    }, {
+                        duration: 300,
+                        easing: 'ease-in-out'
+                    });
+                    a.finished.then(done);
+                }}
+                onEnter={(el, done) => {
+                    const a = el.animate({
+                        opacity: [0, 1],
+                        transform: ['translateY(-5px)', 'translateY(0)'],
+                    }, {
+                        duration: 300,
+                        easing: 'ease-in-out'
+                    });
+                    a.finished.then(done);
+                }}
+            >
+                <Show when={groupType() !== 'normal'}>
+                    <div style={{ display: "flex", "flex-direction": 'column' }}>
+                        <ItemWrap
+                            title={i18n_.rtype[0]}
+                            description={i18n_.rtype[1]}
+                        >
+                            <InputItem
+                                key="ruleType"
+                                value={ruleType()}
+                                type="select"
+                                options={{
+                                    sql: i18n.ruletype.sql,
+                                    backlinks: i18n.ruletype.backlinks,
+                                    attr: i18n.ruletype.attr,
+                                }}
+                                changed={(v) => {
+                                    props.setRule({ type: v });
+                                    setRuleType(v);
+                                }}
+                            />
+                        </ItemWrap>
+                        <ItemWrap
+                            title={i18n_.rinput}
+                            description={aboutRule().desc}
+                            //@ts-ignore
+                            direction={aboutRule().direction}
+                        >
+                            <InputItem
+                                key="ruleInput"
+                                value=''
+                                //@ts-ignore
+                                type={aboutRule().input}
+                                changed={(v) => {
+                                    props.setRule({ input: v });
+                                }}
+                                // nofnSize={true}
+                                flex1={ruleType() === 'attr' ? true : false}
+                            />
+                        </ItemWrap>
+                    </div>
+                </Show>
+            </Transition>
         </div>
     )
 }
