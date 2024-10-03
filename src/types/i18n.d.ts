4a8de1cc33c12f74a0d80d46af23ebf9bc69a56e
frostime <frostime@foxmail.com>
2024-10-03 15:05:18 +0800

    ⚡️ perf: 首次加载优化 #24

---
+++ src/types/i18n.d.ts
@@ -0,0 +1,138 @@
+interface I18n {
+    msg: {
+        ruleFailed: string;
+        ruleInvalid: string;
+        itemHasInGroup: string;
+        itemNotFoundInGroup: string;
+        groupNameEmpty: string;
+    };
+    itemErr: {
+        closed: string;
+        deleted: string;
+    };
+    setting: {
+        replaceDefault: {
+            title: string;
+            description: string;
+        };
+        viewMode: {
+            title: string;
+            description: string;
+        };
+        hideClosed: {
+            title: string;
+            description: string;
+        };
+        hideDeleted: {
+            title: string;
+            description: string;
+        };
+        grouplist: {
+            title: string;
+            description: string;
+        };
+        autoRefreshOnExpand: {
+            title: string;
+            description: string;
+        };
+        ariaLabel: {
+            title: string;
+            description: string;
+        };
+    };
+    viewMode: {
+        bookmark: string;
+        card: string;
+    };
+    bookmark: {
+        new: string;
+        delete: {
+            title: string;
+            desc: string;
+        };
+        cache: string;
+        logo: {
+            name: string;
+            setting: string;
+            add: string;
+            refresh: string;
+            expand: string;
+            collapse: string;
+            min: string;
+        };
+    };
+    group: {
+        msgexist: string;
+        msg404: string;
+        refresh: string;
+        copyref: string;
+        copylink: string;
+        msgcopy: string;
+        docflow: string;
+        rename: string;
+        edit: string;
+        delete: string;
+        move: string;
+        top: string;
+        up: string;
+        down: string;
+        bottom: string;
+        fromclipboard: string;
+        msgparse: string;
+        currentdoc: string;
+        msgdelconfirm: string[];
+    };
+    item: {
+        copyref: string;
+        copylink: string;
+        msgcopy: string;
+        style: string;
+        transfer: string;
+        move: string;
+        top: string;
+        bottom: string;
+        del: string;
+        checkerritem: string;
+        copyitem: string;
+    };
+    newgroup: {
+        name: string[];
+        type: string[];
+        rtype: string[];
+        rinput: string;
+        desc: {
+            sql: string;
+            backlinks: string;
+            attr: string;
+        };
+        choosetemplate: string;
+        postprocess: {
+            ariaLabel: string;
+            name: string;
+            omit: string;
+            fb2p: string;
+            b2doc: string;
+        };
+    };
+    bookmarktype: {
+        normal: string;
+        dynamic: string;
+        composed: string;
+    };
+    ruletype: {
+        sql: string;
+        backlinks: string;
+        attr: string;
+    };
+    template: {
+        sql: {
+            random: string;
+            updated: string;
+            thisday: string;
+            todo: string;
+        };
+        attr: {
+            dailynote: string;
+        };
+    };
+}
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -3,14 +3,13 @@
  * @Author       : frostime
  * @Date         : 2024-07-07 18:57:06
  * @FilePath     : /src/utils/i18n.ts
- * @LastEditTime : 2024-07-07 21:24:01
+ * @LastEditTime : 2024-09-28 22:07:01
  * @Description  : 
  */
-import type I18n from '@/../dev/i18n/zh_CN.json';
 
-export let i18n: typeof I18n = {} as any;
+export let i18n: I18n = {} as any;
 
-export const setI18n = (_i18n_: any) => {
+export const setI18n = (_i18n_: I18n) => {
     i18n = new Proxy(_i18n_, {
         set: (..._) => {
             console.warn("Attempt to modify read-only i18n object");
